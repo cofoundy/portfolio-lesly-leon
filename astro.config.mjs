180dd92bd2cae1cd01d252d9615256eaab962dba
Alvaro Zuñiga <alvaro.gio.zu@gmail.com>
2026-02-19 19:38:56 -0500

    fix: remove base path, set site to custom subdomain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://cofoundy.github.io",
-  base: "/portfolio-lesly-leon",
+  site: "https://lesly-leon.cofoundy.dev",
   vite: {
     plugins: [tailwindcss()],
   },
